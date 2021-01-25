e1835f50206ad3b80eb6fd6e7253eee6007177fe
jadedveldon <sapan.mathur@gmail.com>
2021-01-26 01:41:07 +0530

    Cookoff sol 1

---
+++ cookoff_distinctPairs.py
@@ -0,0 +1,10 @@
+t = int(input())
+for i in range(t):
+    x, y = input().split()
+    x = int(x)
+    y = int(y)
+    z = (2*y)-(2*x)+1
+    print(z)
+
+
+#https://www.codechef.com/COOK126C/problems/MANYSUMS problem
